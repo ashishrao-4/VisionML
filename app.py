44d1b53338fa1ce0c2b60a9d17e0027c7bbbd034
Ashish Rao <95574159+ashishrao-4@users.noreply.github.com>
2024-07-20 15:11:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def download_model():
         return jsonify({'error': 'Trained model not found'}), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", debug=False)
